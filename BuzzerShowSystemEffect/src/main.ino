03b9c9efc9f10af155264720cdb39bb9d1d1877f
Ungedummt <l-schwennesen@gmx.de>
2019-08-01 20:05:51 +0200

    Many changes for Effect

--- BuzzerShowSystemEffect/src/main.ino
+++ BuzzerShowSystemEffect/src/main.ino
@@ -1,30 +1,36 @@
-//Includes-----------------------------------------------------------------------------------
+//----------------------------------------------------------------------------//
+//-------------------------------Includes-------------------------------------//
+//----------------------------------------------------------------------------//
+
 #include <ESP8266WiFi.h>
 #include <ESPDMX.h>
 
-//Variabeln I/O------------------------------------------------------------------------------
+//----------------------------------------------------------------------------//
+//-----------------------------Variables I/O----------------------------------//
+//----------------------------------------------------------------------------//
+
 #define statusLED D7
 
 //Variabeln Sonstige-------------------------------------------------------------------------
-int updateInterval = 20;
-long lastUpdateDMX;
-int StartChannel;
-DMXESPSerial dmx;
+
 
 //DMX Config
-int IPArray[20] = { 100,101,103 };
 int StartChannelNum[20] = { 1,9,18 };
 int BRGBWChannelpostition[5] = { 4,5,6,7,8 }; //das ist die position im bereich wo RGB liegt
-int channelPerEffect = 8;// Rasunehmen aund differenz berechnen únd StopChannelNum einfügen
 int effect[20];
 
-int rgbwRightColor[4] = { 0,255,0,0 }; //w nur falls existent ansonsten 0
-int rgbwWrongColor[4] = { 255,0,0,0 }; //w nur falls existent ansonsten 0
-int rgbwStandbyColor[4] = { 150,150,150,150 }; //w nur falls existent ansonsten 0
-int rgbwPressedPlayerColor[4] = { 255,255,255,255 }; //dies ist die Farbe die bei dem Spieler Erscheint der als Erster Gedrückt hat
-int rgbwOtherPlayersColorRight[4] = { 0,0,0,50 }; //dies ist die Farbe die bei den anderen Spielern Erscheint wenn jemand gedrückt hat
-int rgbwOtherPlayersColorWrong[4] = { 0,0,0,50 }; //dies ist die Farbe die bei den anderen Spielern Erscheint wenn jemand gedrückt hat
-int rgbwNone[4] = { 0,0,0,0 };
+DMXESPSerial dmx;
+int Channelposition;
+int StartChannel = 0;
+
+int brgbwRightColor[5] = { 255,0,255,0,0 }; //w nur falls existent ansonsten 0
+int brgbwWrongColor[5] = { 255,255,0,0,0 }; //w nur falls existent ansonsten 0
+int brgbwStandbyColor[5] = { 255,150,150,150,150 }; //w nur falls existent ansonsten 0
+int brgbwPressedPlayerColor[5] = { 255,255,255,255,255 }; //dies ist die Farbe die bei dem Spieler Erscheint der als Erster Gedrückt hat
+int brgbwOtherPlayersColorRight[5] = { 0,0,0,0,50 }; //dies ist die Farbe die bei den anderen Spielern Erscheint wenn jemand gedrückt hat
+int brgbwOtherPlayersColorWrong[5] = { 0,0,0,0,50 }; //dies ist die Farbe die bei den anderen Spielern Erscheint wenn jemand gedrückt hat
+int BRGBWColor[5];
+const int brgbwNone[5] = { 0,0,0,0,0 };
 
 //Animations------------------------------------------------------------------------------------------------------------------
 int Animation1[4] = { 0  ,0  ,0  ,0 };
@@ -34,158 +40,166 @@ int Animation4[4] = { 115,205,115,115 };
 int Animation5[4] = { 75 ,235,75 ,75 };
 int Animation6[4] = { 0  ,255,0  ,0 };
 
-//Variabeln Network--------------------------------------------------------------------------
+//----------------------------------------------------------------------------//
+//----------------------------Variables Millis--------------------------------//
+//----------------------------------------------------------------------------//
+
+long lastMessage;
+#define MessageInterval 2000
+long lastDisplay = 0;
+#define DisplayInterval 2000
+long lastDMX = 0;
+#define DMXInterval 20
+
+//----------------------------------------------------------------------------//
+//--------------------------Variabeln Network---------------------------------//
+//----------------------------------------------------------------------------//
+
 char* SSID = "Buzzer-AP";           // SSID
 char* KEY = "6732987frkubz3458";    // password
-IPAddress serverIP(192, 168, 4, 1);    // IP address of the AP
-WiFiClient APinput;
-WiFiClient ClientOutput; // APinput name for AP mode
-WiFiServer clientPort(80);
 
-#define type 'E'
+bool SendAllowed = false;
+bool GameisTrue = false;
+bool AnswerTrue = false;
+IPAddress IP_Master(192, 168, 4, 1);     // IP address of the AP
 String IP_long;
-String ClientNumber;
-long lastMessage;
+String IP_short;
+String MAC_Address;
+String MAC_Master;
+WiFiClient Client;
+WiFiServer Port_Master(88);
 
 void setup() {
-	Serial.begin(9600);               // initialization for first 32 addresses by default
-	dmx.init();           // initialization for complete bus
-	//delay(200);
-	//put your setup code here, to run once:
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(SSID, KEY);           // connects to the WiFi AP
-
-	if(WiFi.status() != WL_CONNECTED) {
-	  Serial.println();
-	  Serial.println("Connection to the AP");
-	  connecting();
-	}
-	if(WiFi.status() == WL_CONNECTED){
-	  APinput.connect(serverIP, 80);
-	  IP_long = WiFi.localIP().toString();
-	  ClientNumber = getValue(IP_long, '.', 3);
-	  APinput.print(ClientNumber + ';' + "N" + type + ';' + false + '\r');
-	  Serial.println(ClientNumber + ';' + "N" + type + ';' + false + '\r');
-	  lastMessage = millis();
-	  clientPort.begin();
-	}
+	Prepare();
+	DMX_Setup();
+	WiFi_Setup();
 }
 
 void loop() {
-	//Auf Daten Warten
-	if (clientPort.available()){
-		Serial.print("Nachricht: ");
-		String line = ClientOutput.readStringUntil('\r');
-		Serial.println(line);
-		ClientOutput.flush();
-		ClientOutput.stop();
-	}
-
 	if (WiFi.status() != WL_CONNECTED) {
-		Serial.println();
-		Serial.println("Connection lost retry");
-		connecting();
-		if (WiFi.status() == WL_CONNECTED) {
-			APinput.connect(serverIP, 80);
-			IP_long = WiFi.localIP().toString();
-			ClientNumber = getValue(IP_long, '.', 3);
-			APinput.print(ClientNumber + ';' + "N" + type + ';' + false + '\r');
-			Serial.println(ClientNumber + ';' + "N" + type + ';' + false + '\r');
-			lastMessage = millis();
-		}
+    WiFi_Setup();
 	}
-	// Use WiFiClient class to create TCP connections
-	if ((millis() - lastMessage) >= 2000) {
-		if (WiFi.status() == WL_CONNECTED) {
-			lastMessage = millis();
-			APinput.connect(serverIP, 80);
-			IP_long = WiFi.localIP().toString();
-			ClientNumber = getValue(IP_long, '.', 3);
-			APinput.print(ClientNumber + ';' + type + ';' + false + '\r');
-			Serial.println(ClientNumber + ';' + type + ';' + false + '\r');
-		}else{
-			Serial.println("No connectinon");
-		}
-	}
-	//String SerRead = Serial.readString();
-	// put your main code here, to run repeatedly:
-	delay(5);
 
-	if (Serial.read() == 'G') {
-		UpdateLamp(1, BRGBWChannelpostition, rgbwStandbyColor);// Muss Array mit 5 und 4 stellen sein
-		UpdateLamp(2, BRGBWChannelpostition, rgbwStandbyColor);
+	if ((millis() - lastMessage) >= MessageInterval && SendAllowed == true) {
+		WiFi_Write("SM", "");
+		lastMessage = millis();
 	}
-	if ((lastUpdateDMX + updateInterval) <= millis()) {
-		SendDMX();
-		dmx.update();
-		lastUpdateDMX = millis();
+
+	//DMX_Lamp(1, BRGBWChannelpostition, rgbwRightColor);// Muss Array mit 5 und 4 stellen sein
+	//DMX_Lamp(2, BRGBWChannelpostition, rgbwWrongColor);
+
+	DMX_LampColor(1, BRGBWChannelpostition, "ALL", 255);
+	DMX_LampColor(2, BRGBWChannelpostition, "ALL", 255);
+
+	DMX_Update();
+
+	WiFiClient client = Port_Master.available();
+	if (client) {
+		Serial.print("Nachricht: ");
+		String request = client.readStringUntil('\r');
+		Serial.println("********************************");
+		Serial.println("From the station: " + request);
+		Client.flush();
+		if (request == "AC") {
+			SendAllowed = true;
+		}
 	}
 }
 
+//----------------------------------------------------------------------------//
+//--------------------------------Function DMX--------------------------------//
+//----------------------------------------------------------------------------//
 
-void SendDMX() {
-	for (int x = 1; x < 21; x++) {
-		dmx.write(x, effect[x - 1]);
-		//Serial.print(x);
-		//Serial.print(" / ");
-		//Serial.println(effect[x - 1]);
+void DMX_Setup() {
+	Serial.println("Starting DMX Setup ...");
+	dmx.init();  // initialization for complete bus
+	delay(100);
+	Serial.println("Ending DMX Setup ...");
+}
+
+void DMX_LampColor(int Lamp, int BRGBWPosition[5], String color, int brightness) {
+	int RED = brightness;
+	int GREEN = brightness;
+	int BLUE = brightness;
+	int WHITE = brightness;
+	int NONE = 0;
+	int BRIGHT = 255;
+	if (color == "RESET" || brightness == 0) {
+		int BRGBWColor[5] = {BRIGHT, NONE, NONE, NONE, NONE};
+		DMX_Lamp(Lamp, BRGBWPosition, BRGBWColor);
+	}
+	if (color == "RED") {
+		int BRGBWColor[5] = {BRIGHT, RED, NONE, NONE, NONE};
+		DMX_Lamp(Lamp, BRGBWPosition, BRGBWColor);
+	}
+	if (color == "GREEN") {
+		int BRGBWColor[5] = {BRIGHT, NONE, GREEN, NONE, NONE};
+		DMX_Lamp(Lamp, BRGBWPosition, BRGBWColor);
+	}
+	if (color == "BLUE") {
+		int BRGBWColor[5] = {BRIGHT, NONE, NONE, BLUE, NONE};
+		DMX_Lamp(Lamp, BRGBWPosition, BRGBWColor);
+	}
+	if (color == "PURPLE") {
+		int BRGBWColor[5] = {BRIGHT, RED/2, NONE, BLUE/2, NONE};
+		DMX_Lamp(Lamp, BRGBWPosition, BRGBWColor);
+	}
+	if (color == "YELLOW") {
+		int BRGBWColor[5] = {BRIGHT, RED/2, GREEN/2, NONE, NONE};
+		DMX_Lamp(Lamp, BRGBWPosition, BRGBWColor);
+	}
+	if (color == "AQUA") {
+		int BRGBWColor[5] = {BRIGHT, NONE, GREEN/2, BLUE/2, NONE};
+		DMX_Lamp(Lamp, BRGBWPosition, BRGBWColor);
+	}
+	if (color == "WHITE") {
+		int BRGBWColor[5] = {BRIGHT, NONE, NONE, NONE, WHITE};
+		DMX_Lamp(Lamp, BRGBWPosition, BRGBWColor);
+	}
+	if (color == "ALL") {
+		int BRGBWColor[5] = {BRIGHT, RED, GREEN, BLUE, WHITE};
+		DMX_Lamp(Lamp, BRGBWPosition, BRGBWColor);
+	}
+}
+
+void DMX_Update() {
+	if ((millis() - lastDMX) >= DMXInterval) {
+		DMX_Send();
+		dmx.update();
+		lastDMX = millis();
+	}
+}
+
+void DMX_Send() {
+	for (int x = 1; x < 20; x++) {
+		dmx.write(x, effect[x]);
 	}
 	dmx.update();
 }
 
-void UpdateEffectVar(int channel, int value) {
-	effect[channel] = value;
-	//Serial.print(channel + 1);
-	//Serial.print(" / ");
-	//Serial.println(effect[channel]);
+void UpdateEffectVar(int Channel, int Value) {
+	effect[Channel] = Value;
 }
 
-void UpdateLamp(int LampNum, int BRGBWPosition[5], int RGBWColor[4]) {
-	StartChannel = StartChannelNum[LampNum - 1];
-	//Serial.print(StartChannel);
-	//Serial.print(" / ");
-	//Serial.println(LampNum);
-	if (BRGBWPosition[0] != 0) {
-		UpdateEffectVar((StartChannel + BRGBWPosition[0] - 2), 255);
-	}
-	for (int i = 0; i < 4; i++) {
-		if (BRGBWPosition[0] == 0) {
-			int Channelposition = BRGBWPosition[i + 1];
+void DMX_Lamp(int Lamp, int BRGBWPosition[5], int BRGBWColor[5]) {
+	for (int i = 0; i < 5; i++) {
+		Channelposition = BRGBWPosition[i];
+		if (Channelposition != 0) {
+			int Channel = (StartChannelNum[Lamp - 1] + Channelposition - 1);
+			int Color = BRGBWColor[i];
+			UpdateEffectVar(Channel, Color);
 		}
-		int Channelposition = BRGBWPosition[i + 1];
-		int Channel = (StartChannel + Channelposition - 2);
-		int Color = RGBWColor[i];
-		UpdateEffectVar(Channel, Color);
 	}
 }
 
-void connecting() {
-	while (WiFi.status() != WL_CONNECTED) {
-		Serial.print(".");
-		analogWrite(statusLED, 150);
-		delay(250);
-		analogWrite(statusLED, 0);
-		delay(250);
-	}
-	analogWrite(statusLED, 50);
-
-	Serial.println();
-	Serial.println("Connected");
-	Serial.println("station_bare_01.ino");
-	Serial.println("LocalIP:" + WiFi.localIP());
-	Serial.println("MAC:" + WiFi.macAddress());
-	Serial.println("Gateway:" + WiFi.gatewayIP());
-	Serial.println("AP MAC:" + WiFi.BSSIDstr());
-	IP_long = WiFi.localIP().toString();
-	ClientNumber = getValue(IP_long, '.', 3);
-	Serial.println("Client Number: " + ClientNumber);
-}
+//----------------------------------------------------------------------------//
+//--------------------------------Functions-----------------------------------//
+//----------------------------------------------------------------------------//
 
 String getValue(String data, char separator, int index) {
 	int found = 0;
 	int strIndex[] = { 0, -1 };
 	int maxIndex = data.length() - 1;
-
 	for (int i = 0; i <= maxIndex && found <= index; i++) {
 		if (data.charAt(i) == separator || i == maxIndex) {
 			found++;
@@ -194,4 +208,55 @@ String getValue(String data, char separator, int index) {
 		}
 	}
 	return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void Prepare() {  // for pinModes and more .....
+	Serial.begin(9600);
+}
+
+//----------------------------------------------------------------------------//
+//---------------------------------WiFi---------------------------------------//
+//----------------------------------------------------------------------------//
+
+void WiFi_Setup() {
+  WiFi.mode(WIFI_STA);
+	WiFi.begin(SSID, KEY);           // connects to the WiFi AP
+  while (WiFi.status() != WL_CONNECTED) {
+		Serial.print(".");
+		delay(1000);
+	}
+  IP_long = WiFi.localIP().toString();
+  IP_short = getValue(IP_long, '.', 3);
+  MAC_Address = WiFi.macAddress();
+  MAC_Master = WiFi.BSSIDstr();
+	Serial.println();
+	Serial.println("Connected");
+	Serial.println("station_bare_01.ino");
+	Serial.println("LocalIP: " + IP_long);
+  Serial.println("LocalID: " + IP_short);
+	Serial.println("MAC: " + MAC_Address);
+	Serial.println("IP  Master: " + WiFi.gatewayIP());
+	Serial.println("MAC Master: " + MAC_Master);
+	Port_Master.begin();
+  WiFi_Write("NE", MAC_Address);
+  lastMessage = millis();
+}
+
+void WiFi_Write(String MessageType, String Parameters){
+  if (WiFi.status() == WL_CONNECTED) {
+		Client.connect(IP_Master, 80);
+		IP_long = WiFi.localIP().toString();
+		IP_short = getValue(IP_long, '.', 3);
+    if (Parameters != NULL || Parameters != "") {
+      Client.print(MessageType + ";" + IP_short + ";" + Parameters + '\r');
+      Serial.println(MessageType + ";" + IP_short + ";" + Parameters + '\r');
+    }
+    else{
+      Client.print(MessageType + ";" + IP_short + '\r');
+      Serial.println(MessageType + ";" + IP_short + '\r');
+    }
+	}
+	else {
+		Serial.println("No connectinon");
+  }
 }
